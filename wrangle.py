2bdf38fef2e7adac0839ea7f945175bffb6ccac0
joann <joann.balraj@gmail.com>
2021-11-30 17:15:35 -0600

    11.30

---
+++ wrangle.py
@@ -0,0 +1,135 @@
+#data wrangling functions for regression exercises - Aquisition & Prep
+
+import pandas as pd
+
+from env import user, password, host
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def acquire_zillow():
+    ''' Acquire data from Zillow using env imports and rename columns'''
+    
+    url = f"mysql+pymysql://{user}:{password}@{host}/zillow"
+    
+    query = """
+            
+    SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    FROM properties_2017
+
+    LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+
+    WHERE propertylandusedesc IN ("Single Family Residential",                       
+                                  "Inferred Single Family Residential")"""
+
+    # get dataframe of data
+    df = pd.read_sql(query, url)
+    
+    
+    # renaming column names to one's I like better
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built',})
+    return df
+
+
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+
+
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount'])
+    
+    # get distributions of numeric data
+    get_hist(df)
+    
+    # converting column datatypes
+    df.year_built = df.year_built.astype(object)
+    
+    # drop taxamount
+    df = df.drop(columns = 'taxamount')
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute year built using mode
+    imputer = SimpleImputer(strategy='most_frequent')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test
+
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test 
